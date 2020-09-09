d5077ae3cf922c27addb37543544e604454a5fca
René <snooz@posteo.de>
2020-09-09 11:55:08 +0200

    Added Apache 2.0 comments

--- sources/src/components/CommunityBlock.vue
+++ sources/src/components/CommunityBlock.vue
@@ -1,3 +1,21 @@
+<!--
+Copyright 2020 -     Robot Framework Foundation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This code was derived from https://github.com/robotframework/robotframework.github.com
+-->
+
 <template>
   <div class="row">
         <b-tabs pills class="nav-fill col-lg-12 m-3 mt-5 mb-5">
